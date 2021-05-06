e540544198170586be120443fe9e07761f441e5b
Chris Conner <chris.conner57@gmail.com>
2021-05-06 08:13:54 -0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Chris Conner on 5/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
