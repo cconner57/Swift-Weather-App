38433e353e50f51109475075d850914270f31cf6
Chris Conner <chris.conner57@gmail.com>
2021-05-10 21:00:12 -0700

    Finish WeatherData Model

--- Weather/Models/Weather.swift
+++ Weather/Models/Weather.swift
@@ -1,101 +1,34 @@
 import Foundation
 
-class Weather: Decodable, Identifiable {
-	
+struct WeatherData: Decodable {
+	var weather: [Weather]
+	var main: Main
+	var visibility: Int
+	var wind: Wind
+	var sys: Country
+	var name: String
+}
+
+struct Weather: Decodable {
+	var ida: Int
 	var main: String
 	var description: String
-	var temp: Int
-	var name: String
-	var humidity: String
-	var wind: String
-	var air: String
-	var uv: String
-	var tempMin: String
-	var tempMax: String
-
-	enum CodingKeys: String, CodingKey {
-		
-		case imageUrl = "image_url"
-		case isClosed = "is_closed"
-		case reviewCount = "review_count"
-		case displayPhone = "display_phone"
-		
-		case id
-		case alias
-		case name
-		case url
-		case categories
-		case rating
-		case coordinates
-		case transactions
-		case price
-		case location
-		case phone
-		case distance
-	}
-	
-	func getImageData() {
-		
-		// Check that image url isn't nil
-		guard imageUrl != nil else {
-			return
-		}
-		
-		// Download the data for the image
-		if let url = URL(string: imageUrl!) {
-			
-			// Get a session
-			let session = URLSession.shared
-			let dataTask = session.dataTask(with: url) { (data, response, error) in
-				
-				if error == nil {
-					
-					DispatchQueue.main.async {
-						// Set the image data
-						self.imageData = data!
-					}
-				}
-			}
-			dataTask.resume()
-		}
-	}
-	
-	static func getTestData() -> Business {
-		let b = Business()
-		return b
-	}
 }
 
-struct Location: Decodable {
-	var address1: String?
-	var address2: String?
-	var address3: String?
-	var city: String?
-	var zipCode: String?
-	var country: String?
-	var state: String?
-	var displayAddress: [String]?
-	
-	enum CodingKeys: String, CodingKey {
-		case zipCode = "zip_code"
-		case displayAddress = "display_address"
-		
-		case address1
-		case address2
-		case address3
-		case city
-		case country
-		case state
-		
-	}
+struct Main: Decodable {
+	var temp: Double
+	var temp_min: Double
+	var temp_max: Double
+	var pressure: Int
+	var humidity: Int
 }
 
-struct Category: Decodable {
-	var alias: String?
-	var title: String?
+struct Wind: Decodable {
+	var speed: Double
 }
 
-struct Coordinate: Decodable {
-	var latitude: Double?
-	var longitude: Double?
+struct Country: Decodable {
+	var country: String
+	var sunrise: Int
+	var sunset: Int
 }
--- Weather/Views/ContentView.swift
+++ Weather/Views/ContentView.swift
@@ -2,30 +2,50 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	//api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+	@EnvironmentObject var model: ContentModel
+	
+	func findWeatherImage(main: String) -> Any {
+		switch main {
+		case "Clear" :
+			return Image(systemName: "sun.min.fill")
+		case "Thunderstorm" :
+			return Image(systemName: "sun.min.fill")
+		case "Drizzle" :
+			return Image(systemName: "sun.min.fill")
+		case "Rain" :
+			return Image(systemName: "sun.min.fill")
+		case "Snow" :
+			return Image(systemName: "sun.min.fill")
+		case "Clouds" :
+			return Image(systemName: "sun.min.fill")
+		default:
+			return Image(systemName: "sun.min.fill")
+		}
+	}
 	
 	var body: some View {
 		
 		VStack {
 			Text(Date(), style: .date)
 				.font(.largeTitle)
-				.padding(.top, 60)
+				.padding(.top, 50)
 			GeometryReader	{ geometry in
 				VStack {
 					HStack(spacing: 60) {
 						VStack(alignment: .leading) {
-							Text("Los Angeles")
+							Text(model.weatherSpecs.name)
 								.font(.title2)
 							Text("78°")
 								.font(.largeTitle)
 						}
 						.padding()
+						
 						Image(systemName: "sun.min.fill")
 							.foregroundColor(Color(.systemYellow))
 							.font(.system(size: 100))
 							.padding()
 					}
-					.frame(height: geometry.size.height * 0.23)
+					.frame(height: geometry.size.height * 0.20)
 					HStack(spacing: 28) {
 						WeekItem(date: "Now", temp: "78")
 						WeekItem(date: "12pm", temp: "78")
@@ -34,7 +54,7 @@ struct ContentView: View {
 						WeekItem(date: "3pm", temp: "78")
 					}
 					.font(.headline)
-					.frame(height: geometry.size.height * 0.22)
+					.frame(height: geometry.size.height * 0.26)
 					HStack(spacing: 32) {
 						VStack(alignment: .leading) {
 							Text("Humidity")
@@ -70,7 +90,7 @@ struct ContentView: View {
 					.background(Color(hue: 0.569, saturation: 0.8, brightness: 1, opacity: 1))
 					.cornerRadius(25)
 					.font(.title3)
-					.frame(height: geometry.size.height * 0.50)
+					.frame(height: geometry.size.height * 0.53)
 				}
 			}
 			
